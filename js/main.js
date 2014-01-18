3377411df75cedce2560d6161e6115249f3ff9f9
Merfoo <fauzimerfoo@gmail.com>
2014-01-18 14:29:45 -0800

    Works, code was not being be called

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
 // Get the Canvas element from our HTML below
-var canvas = document.getElementById("renderCanvas");
+var canvas;
 
 // Load BABYLON 3D engine and set the root directory
-var engine = new BABYLON.Engine(canvas, true);
+var engine;
 
 //Create a new scene with a camera (mandatory), a light (better) and a sphere (to see the origin)
-var scene = new BABYLON.Scene(engine);
+var scene;
 
 // Creating a camera looking to the zero point (0,0,0)
-var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
+var camera;
 
 // Creating a omnidirectional light
-var light0 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 10), scene);
+var light0;
 
 // Creating a sphere of size 1, at 0,0,0
-var origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
+var origin;
+    
+function init()
+{   
+    // Get the Canvas element from our HTML below
+    canvas = document.getElementById("renderCanvas");
 
-// Attach the camera to the scene
-scene.activeCamera.attachControl(canvas);
+    // Load BABYLON 3D engine and set the root directory
+    engine = new BABYLON.Engine(canvas, true);
 
-// Once the scene is loaded, just register a render loop to render it
-engine.runRenderLoop(function () {
-    scene.render();
-});
+    //Create a new scene with a camera (mandatory), a light (better) and a sphere (to see the origin)
+    scene = new BABYLON.Scene(engine);
+
+    // Creating a camera looking to the zero point (0,0,0)
+    camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
+
+    // Creating a omnidirectional light
+    light0 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 10), scene);
+    
+    // Creating a sphere of size 1, at 0,0,0
+    origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
+
+    // Attach the camera to the scene
+    scene.activeCamera.attachControl(canvas);
+    
+    // Once the scene is loaded, just register a render loop to render it
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+}
