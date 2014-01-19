b1ec346f529b59efa7c843096562ccbb8a373b8a
dlkulp <damiankulp@gmail.com>
2014-01-18 16:12:45 -0800

    more classes
    added more and am putting lots of comments in

--- js/class.js
+++ js/class.js
@@ -1,6 +1,7 @@
+//handle inheritance because javascript doesn't
 inheritanceManager = {};
 
-inheritanceManager.extend = function (subClass, baseClass)
+inheritanceManager.extend = function( subClass, baseClass )
 {
     function inheritance() {}
     inheritance.prototype = baseClass.prototype;
@@ -8,4 +9,93 @@ inheritanceManager.extend = function (subClass, baseClass)
     subClass.prototype.constructor = subClass;
     subClass.baseConstructor = baseClass;
     subClass.superClass = baseClass.prototype;
+}
+
+//---------------
+//parent classes
+//---------------
+
+//anything that moves will extend moving objects
+//input: x,y,z - location; 
+MovingObject = function( x, y, z, vx, vy, vz, rx, ry, rz )
+{
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.vx = vx;
+    this.vy = vy;
+    this.vz = vz;
+    this.rx = rx;
+    this.ry = ry;
+    this.rz = rz;
+}
+
+StaticObject = function( x, y, z )
+{
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
+//---------------
+//children classes
+//---------------
+
+Asteroid = function( x, y, z, vx, vy, vz, rx, ry, rz )
+{
+    inheritanceManager.extend(this, MovingObject);
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.vx = vx;
+    this.vy = vy;
+    this.vz = vz;
+    this.rx = rx;
+    this.ry = ry;
+    this.rz = rz;
+}
+
+Ship = function( x, y, z, vx, vy, vz, rx, ry, rz )
+{
+    inheritanceManager.extend(this, MovingObject);
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.vx = vx;
+    this.vy = vy;
+    this.vz = vz;
+    this.rx = rx;
+    this.ry = ry;
+    this.rz = rz;
+
+    this.bIsShooting = false;
+    this.health = 100;
+    this.shield = 100;
+    this.ammo = 100;
+    this.lives = 3;
+}
+
+Laser = function( x, y, z, vx, vy, vz, r )
+{
+    inheritanceManager.extend(this, MovingObject);
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.vx = vx;
+    this.vy = vy;
+    this.vz = vz;
+    this.r = r;
+
+    this.bIsHit = false;
+}
+
+Pickup = function( x, y, z, type, amount )
+{
+    inheritanceManager.extend(this, StaticObject);
+    this.x = x;
+    this.y = y;
+    this.z = z;
+
+    this.type = type;
+    this.amount = amount;
 }
